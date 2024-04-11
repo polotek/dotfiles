49ca5c44cabca7041601ef75472f08a5c7a4103f
Marco Rogers <marco.rogers@gmail.com>
2024-04-11 14:31:04 -0700

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,27 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-source ~/.aliases
+###################################################
+# Variables
+###################################################
+
+# Don't put duplicate lines in the history.
+set -gx HISTCONTROL ignoredups
+# huge hist files aren't a problem
+set -gx HISTFILESIZE 1000000
+# and huge history lists are very useful
+set -gx HISTSIZE 10000
+
+fish_add_path $HOME/bin
+
+###################################################
+# Other settings
+###################################################
+
+source $HOME/.aliases
+
+###################################################
+# Variables
+###################################################
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
